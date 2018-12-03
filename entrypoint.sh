292944738e91376505cb7f2219a42ed559b0ad08
ggggg <you@example.com>
2018-12-03 15:39:48 +0000

    ddd

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+DTR_IP=3.121.87.103
+openssl s_client -connect ${DTR_IP}:443 -showcerts \
+    </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee \
+    /usr/local/share/ca-certificates/${DTR_IP}.crt
+sudo update-ca-certificates
+
+/bin/tini -- /usr/local/bin/jenkins.sh
